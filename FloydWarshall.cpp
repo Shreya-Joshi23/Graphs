d7268c195ce6194651f58cde588900c75a986bc4
Shreya <joshi2003shreya@gmail.com>
2023-09-28 17:52:54 +0530

    Graphs

--- FloydWarshall.cpp
+++ FloydWarshall.cpp
@@ -1,34 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> floydWarshall(int V,vector<vector<int>>adj,int src){
+//multisource shortest path algorithm
+// does not work for the graphs with negative cycles 
+//Time complexity - O(N^3)
 
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(adj[i][j]==-1){
-                    adj[i][j]=1e9;
+vector<vector<int>> floydWarshall(int V, vector<vector<int>> adj, int src)
+{
+
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (adj[i][j] == -1)
+            {
+                adj[i][j] = 1e9;
+            }
+        }
+    }
+
+    for (int via = 0; via < V; via++)
+    {
+        for (int src = 0; src < V; src++)
+        {
+            for (int des = 0; des < V; des++)
+            {
+                if ((adj[src][via] + adj[via][des]) < adj[src][des])
+                {
+                    adj[src][des] = adj[src][via] + adj[via][des];
                 }
             }
         }
+    }
 
-        for(int via=0;via<V;via++){
-            for(int src=0;src<V;src++){
-                for(int des=0;des<V;des++){
-                    if((adj[src][via]+adj[via][des])<adj[src][des]){
-                        adj[src][des]=adj[src][via]+adj[via][des];
-                    }
-                }
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (adj[i][j] == 1e9)
+            {
+                adj[i][j] = -1;
             }
         }
+    }
 
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(adj[i][j]==1e9){
-                    adj[i][j]=-1;
-                }
-            }
-        }
-
-        return adj;
-
+    return adj;
 }
